f82529fd5c121aeaa3a5e92e64e216e980f2064b
Michael Riffle <mriffle@uw.edu>
2021-05-11 13:43:07 -0700

    Change default cutoffs to 0.01 (psm) and 0.01 (peptide)

--- src/main/java/org/yeastrc/limelight/xml/comet_percolator/annotation/PeptideAnnotationTypes.java
+++ src/main/java/org/yeastrc/limelight/xml/comet_percolator/annotation/PeptideAnnotationTypes.java
@@ -45,7 +45,7 @@ public class PeptideAnnotationTypes {
 				type.setName( PERCOLATOR_ANNOTATION_TYPE_QVALUE );
 				type.setDescription( "Q-value" );
 				type.setFilterDirection( FilterDirectionType.BELOW );
-				type.setDefaultFilterValue( BigDecimal.valueOf( 0.05 ) );
+				type.setDefaultFilterValue( BigDecimal.valueOf( 0.01 ) );
 	
 				types.add( type );
 			}
